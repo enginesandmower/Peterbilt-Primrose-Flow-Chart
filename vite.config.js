9acd1347ce2eeb2e0d0d7a5691511f9438fd7910
enginesandmower <enginesandmower@hotmail.com>
2025-10-15 07:58:24 -0500

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173, host: true },
-  preview: { port: 4173, host: true },
+  preview: { 
+    port: 4173, 
+    host: true,
+    allowedHosts: ['peterbilt-primrose-flow-chart.onrender.com']
+  }
 });
